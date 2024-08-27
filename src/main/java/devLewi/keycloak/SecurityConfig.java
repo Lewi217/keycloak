08be105fa747de271c15259f8d2c1eb74efa01e4
wanjohi lewis <wanjohilewi7@gmail.com>
2024-08-27 14:25:22 +0300

    security config added

--- src/main/java/devLewi/keycloak/SecurityConfig.java
+++ src/main/java/devLewi/keycloak/SecurityConfig.java
@@ -3,9 +3,11 @@ package devLewi.keycloak;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -17,22 +19,15 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated();
-
-        http
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthConverter);
-
-        http
-                .sessionManagement()
-                .sessionCreationPolicy(STATELESS);
+                .csrf(csrf -> csrf.disable()) // Updated way to disable CSRF
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().authenticated()) // Updated method for authorizing HTTP requests
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults())) // Updated method for configuring JWT
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Updated method for session management
 
         return http.build();
     }
