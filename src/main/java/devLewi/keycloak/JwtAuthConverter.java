9187632e374095a7a881c0ff7d1ff21de2d4c039
wanjohi lewis <wanjohilewi7@gmail.com>
2024-09-04 14:44:05 +0300

    initialisation of JwtAuthConverter

--- src/main/java/devLewi/keycloak/JwtAuthConverter.java
+++ src/main/java/devLewi/keycloak/JwtAuthConverter.java
@@ -1,4 +1,42 @@
 package devLewi.keycloak;
 
-public class JwtAuthConverter {
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+            new JwtGrantedAuthoritiesConverter();
+
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
+        var authorities  = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()
+        );
+        return null;
+    }
+
+    private Collection<?extends GrantedAuthority> extractResourceRoles(Jwt jwt){
+        Map<String, Object> resourceAccess;
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if(jwt.getClaim("resource_access") == null){
+            return Set.of();
+        }
+        resourceAccess = jwt.getClaim("resource_access");
+        return null;
+    }
 }
